c29e05a848a4e3404b19b94c0935370dcfc2be11
Suprun Yakov <yakovsuprun@yandex.ru>
2025-07-15 15:29:51 +0300

    Add PATCH and DELETE handlers

--- main.go
+++ main.go
@@ -41,6 +41,36 @@ func postTask(c echo.Context) error {
 	return c.JSON(http.StatusCreated, task)
 }
 
+func patchTask(c echo.Context) error {
+	id := c.Param("id")
+
+	var req TaskRequest
+	if err := c.Bind(&req); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid request"})
+	}
+
+	for i, task := range tasks {
+		if task.ID == id {
+			tasks[i].Name = req.Name
+			tasks[i].Status = req.Status
+			return c.JSON(http.StatusOK, tasks[i])
+		}
+	}
+	return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid request"})
+}
+
+func deleteTask(c echo.Context) error {
+	id := c.Param("id")
+
+	for i, task := range tasks {
+		if task.ID == id {
+			tasks = append(tasks[:i], tasks[i+1:]...)
+			return c.NoContent(http.StatusNoContent)
+		}
+	}
+	return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid request"})
+}
+
 func main() {
 
 	e := echo.New()
@@ -50,6 +80,8 @@ func main() {
 
 	e.GET("/tasks", getTask)
 	e.POST("/tasks", postTask)
+	e.PATCH("/tasks/:id", patchTask)
+	e.DELETE("/tasks/:id", deleteTask)
 
 	e.Start("localhost:8080")
 }
